20ceacabb1df3988b9fae9605a3f49648f5d72fa
Brian Berns <brianberns@gmail.com>
2022-10-18 15:02:31 -0400

    Tokenizer.

---
+++ Assignment1.Test/TokTests.fs
@@ -0,0 +1,18 @@
+namespace Assignment1
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TokTests() =
+
+    [<TestMethod>]
+    member _.``(a b)``() =
+        let expected =
+            [
+                LPAREN (0, 0, 0, 1)
+                TSym ("a", (0, 1, 0, 2))
+                TSym ("b", (0, 3, 0, 4))
+                RPAREN (0, 4, 0, 5)
+            ]
+        let actual = Tok.tokenize "(a b)"
+        Assert.AreEqual(expected, actual)
---
+++ Assignment1/Tok.fs
@@ -0,0 +1,74 @@
+﻿namespace Assignment1
+
+open FParsec
+
+type tok<'a> =
+    | LPAREN of 'a
+    | RPAREN of 'a
+    | TSym of string * 'a
+    | TInt of int * 'a
+    | TBool of bool * 'a
+
+(* startline, startcol, endline, endcol *)
+type pos = int * int * int * int
+
+module Tok =
+
+    let private parsePos p makeToken : Parser<_, unit> =
+        parse {
+            let! startPos = getPosition
+            let! value = p
+            let! endPos = getPosition
+            let pos : pos =
+                int startPos.Line - 1,
+                int startPos.Column - 1,
+                int endPos.Line - 1,
+                int endPos.Column - 1
+            return makeToken (value, pos)
+        }
+
+    let private parseLparen =
+        parsePos
+            (skipChar '(')
+            (snd >> LPAREN)
+
+    let private parseRparen =
+        parsePos
+            (skipChar ')')
+            (snd >> RPAREN)
+
+    let private parseSym =
+        let psym = identifier (IdentifierOptions ())
+        parsePos psym TSym
+
+    let private parseInt =
+        parsePos pint32 TInt
+
+    let private parseBool =
+        let pbool =
+            choice [
+                skipStringCI "true" >>% true
+                skipStringCI "false" >>% false
+            ]
+        parsePos pbool TBool
+
+    let private parseTok =
+        choice [
+            parseLparen
+            parseRparen
+            parseSym
+            parseInt
+            parseBool
+        ] .>> spaces
+
+    let private parseText =
+        spaces
+            >>. many parseTok
+            .>> spaces
+            .>> eof
+
+    /// Question 3.
+    let tokenize text =
+        match runParserOnString parseText () "" text with
+            | Success (result, _, _) -> result
+            | Failure (msg, _, _) -> failwith msg
