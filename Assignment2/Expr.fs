47303e0640bca0c03a21b791fd28ceafeba347c8
Brian Berns <brianberns@gmail.com>
2022-10-21 15:27:34 -0400

    Polish.

--- Assignment2/Expr.fs
+++ Assignment2/Expr.fs
@@ -4,25 +4,24 @@ open Microsoft.CodeAnalysis
 open Microsoft.CodeAnalysis.CSharp
 open type SyntaxFactory
 
-open Assignment1
+open Assignment1   // for S-expression parser
 
-type prim1 =
-    | Add1
-    | Sub1
-
+/// Abstract syntax for the Adder language.
 type expr<'a> =
     | Number of int64 * 'a
     | Prim1 of prim1 * expr<'a> * 'a
-    | Let of (*bindings*) List<string * expr<'a>> * expr<'a> * 'a
-    | Id of string * 'a
+    | Let of
+        bindings : List<string * expr<'a>>
+            * expr<'a>
+            * 'a
+    | Id of name : string * 'a
 
-type env = Map<string, Syntax.ExpressionSyntax>
+and prim1 =
+    | Add1
+    | Sub1
 
-module Env =
-
-    let add name node (env : env) : env =
-        Map.add name node env
-
+/// Standard return type for compiler results. (This is
+/// preferable to throwing exceptions.)
 type CompilerResult<'a> = Result<'a, string[]>
 
 [<AutoOpen>]
@@ -31,6 +30,27 @@ module private CompilerResult =
     let error<'a> msg : CompilerResult<'a> =
         Error [| msg |]
 
+/// Variable environment.
+type env = Map<string, Syntax.ExpressionSyntax>
+
+module Env =
+
+    let tryAdd name node (env : env) : CompilerResult<env> =
+        if Map.containsKey name env then
+            error $"Variable already exists: {name}"
+        else
+            Ok (Map.add name node env)
+
+    let tryFind name (env : env) =
+        match Map.tryFind name env with
+            | Some node -> Ok node
+            | None -> error $"Unbound identifier: {name}"
+
+/// Functions for converting expressions into Rosyln syntax
+/// nodes. (It would've be nice to generate a .NET assembly
+/// by emitting CIL opcodes instead, but there's currently
+/// no reliable way to save such an assembly to a file.
+/// See https://github.com/dotnet/runtime/issues/15704.
 module Expr =
 
     let private numericLiteral (n : int64) =
@@ -38,36 +58,17 @@ module Expr =
             SyntaxKind.NumericLiteralExpression,
             Literal(n))
 
-    let private declareVariable id node =
-        LocalDeclarationStatement(
-            VariableDeclaration(
-                IdentifierName(
-                    Identifier(
-                        TriviaList(),
-                        SyntaxKind.VarKeyword,
-                        "var",
-                        "var",
-                        TriviaList())))
-                .WithVariables(
-                    SingletonSeparatedList(
-                        VariableDeclarator(
-                            Identifier(id))
-                            .WithInitializer(
-                                EqualsValueClause(
-                                    node)))))
-
     let private compileNumber n (env : env) =
         let node =
             numericLiteral n
                 :> Syntax.ExpressionSyntax
         Ok (node, env)
 
-    let private compileId id (env : env) =
-        match Map.tryFind id env with
-            | Some node -> Ok (node, env)
-            | None -> error $"Unbound identifier: {id}"
+    let private compileId name env =
+        Env.tryFind name env
+            |> Result.map (fun node -> node, env)
 
-    let rec private compileExp exp (env : env) : Result<Syntax.ExpressionSyntax * env, _> =
+    let rec private compileExp exp env =
         match exp with
             | Number (n, _) -> compileNumber n env
             | Prim1 (op, e, _) -> compilePrim1 op e env
@@ -96,7 +97,7 @@ module Expr =
                 | (name, exp) :: tail ->
                     result {
                         let! node, env' = compileExp exp env
-                        let env'' = env' |> Env.add name node
+                        let! env'' = env' |> Env.tryAdd name node
                         return! loop tail env''
                     }
                 | [] -> Ok env
@@ -106,8 +107,10 @@ module Expr =
             return! compileExp exp env'
         }
 
-    let compile exp =
+    /// Corresponds roughly to `compile : pos expr -> instruction list`
+    /// in the assignment.
+    let compile exp : CompilerResult<_> =
         result {
-            let! node, env = compileExp exp Map.empty
+            let! node, _ = compileExp exp Map.empty
             return node
         }
