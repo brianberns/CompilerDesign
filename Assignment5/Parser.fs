955b2098ed048375b2fee5dce3725a51ca6dc360
Brian Berns <brianberns@gmail.com>
2022-10-27 14:53:50 -0400

    More work on assignment 5.

--- Assignment5/Parser.fs
+++ Assignment5/Parser.fs
@@ -17,9 +17,6 @@ module Parser =
             skipSatisfy Char.IsWhiteSpace
                 <|> skipComment)
 
-    let private parseExpr, parseExprRef =
-        createParserForwardedToRef ()
-
     let private parsePos create parser =
         parse {
             let! startPos = getPosition
@@ -28,43 +25,16 @@ module Parser =
             return create value (startPos, endPos)
         }
 
-    let private parseNumber : Parser<_, unit> =
-        pint32
-            |> parsePos (fun n tag ->
-                NumberExpr {
-                    Number = n
-                    Tag = tag
-                })
-
     let private parseIdentifierName =
         identifier (IdentifierOptions ())
             |> parsePos (fun ident tag ->
                 (ident, tag))
-
-    let private parseIdentifier =
-        parseIdentifierName
-            |>> (fun (ident, tag) ->
-                IdentifierExpr {
-                    Identifier = ident
-                    Tag = tag
-                })
 
     let private choiceF pairs =
         pairs
             |> Seq.map (fun (str, f) ->
                 skipString str >>% f)
             |> choice
-
-    let private parseBool =
-        choiceF [
-            "true", true
-            "false", false
-        ]
-            |> parsePos (fun flag tag ->
-                BoolExpr {
-                    Flag = flag
-                    Tag = tag
-                })
 
     let private parseParens parser =
         parse {
@@ -74,135 +44,171 @@ module Parser =
             return value
         }
 
-    let private parsePrim1 =
-        parse {
-            let! op = 
-                choiceF [
-                    "add1", Add1
-                    "sub1", Sub1
-                    "print", Print
-                    "isbool", IsBool
-                    "isnum", IsNum
-                    "!", Not
-                ]
-            do! spaces
-            let! expr = parseParens parseExpr
-            return op, expr
-        } |> parsePos (fun (op, expr) tag ->
-            Prim1Expr {
-                Operator = op
-                Expr = expr
-                Tag = tag
-            })
+    module private Expr =
 
-    let private parseIf =
-        parse {
-            do! skipString "if" >>. spaces
-            let! cond = parseExpr
-            do! spaces >>. skipChar ':' >>. spaces
-            let! trueBranch = parseExpr
-            do! spaces >>. skipString "else:" >>. spaces
-            let! falseBranch = parseExpr
-            return cond, trueBranch, falseBranch
-        } |> parsePos (fun (cond, trueBranch, falseBranch) tag ->
-            IfExpr {
-                Condition = cond
-                TrueBranch = trueBranch
-                FalseBranch = falseBranch
-                Tag = tag
-            })
+        let private parseExpr, parseExprRef =
+            createParserForwardedToRef ()
 
-    let private parseBinding =
-        parse {
-            let! (ident, tag) = parseIdentifierName
-            do! spaces >>. skipChar '=' >>. spaces
-            let! expr = parseExpr
-            return {
-                Identifier = ident
-                Expr = expr
-                Tag = tag   // identifier tag, not for entire binding
-            }
-        }
+        let private parseNumber : Parser<_, unit> =
+            pint32
+                |> parsePos (fun n tag ->
+                    NumberExpr {
+                        Number = n
+                        Tag = tag
+                    })
 
-    let private parseBindings =
-        sepBy1
-            (parseBinding .>> spaces)
-            (skipChar ',' .>> spaces)
+        let private parseIdentifier =
+            parseIdentifierName
+                |>> (fun (ident, tag) ->
+                    IdentifierExpr {
+                        Identifier = ident
+                        Tag = tag
+                    })
 
-    let private parseLet =
-        parse {
-            do! skipString "let" >>. spaces
-            let! bindings = parseBindings
-            do! spaces >>. skipString "in" >>. spaces
-            let! expr = parseExpr
-            return bindings, expr
-        } |> parsePos (fun (bindings, expr) tag ->
-            LetExpr {
-                Bindings = bindings
-                Expr = expr
-                Tag = tag
-            })
-
-    let private parseArguments =
-        sepBy
-            (parseExpr .>> spaces)
-            (skipChar ',' >>. spaces)
-
-    let parseApplication =
-        parse {
-            let! (ident, _) = parseIdentifierName
-            do! spaces
-            let! args = parseParens parseArguments
-            return ident, args
-        }
-            |> parsePos (fun (ident, args) tag ->
-                ApplicationExpr {
-                    Identifier = ident
-                    Arguments = args
-                    Tag = tag   // should we use the identifier's tag instead?
-                })
-            |> attempt          // rollback if needed
-
-    let private parseSimpleExpr =
-        choice [
-            parseNumber
-            parseBool
-            parsePrim1
-            parseIf
-            parseLet
-            parseApplication
-            parseIdentifier   // must come after other parsers
-            parseParens parseExpr
-        ]
-
-    let private parseExprImpl =
-        let create op left right =
-            Prim2Expr {
-                Operator = op
-                Left = left
-                Right = right
-                Tag = fst left.Tag', snd right.Tag'
-            }
-        let parseOp =
+        let private parseBool =
             choiceF [
-                "+", create Plus
-                "-", create Minus
-                "*", create Times
-                "&&", create And
-                "||", create Or
-                ">=", create GreaterEq   // must come before ">"
-                ">", create Greater
-                "<=", create LessEq      // must come before "<"
-                "<", create Less
-                "==", create Eq
+                "true", true
+                "false", false
             ]
-        chainl1
-            (parseSimpleExpr .>> spaces)
-            (parseOp .>> spaces)
+                |> parsePos (fun flag tag ->
+                    BoolExpr {
+                        Flag = flag
+                        Tag = tag
+                    })
+
+        let private parsePrim1 =
+            parse {
+                let! op = 
+                    choiceF [
+                        "add1", Add1
+                        "sub1", Sub1
+                        "print", Print
+                        "isbool", IsBool
+                        "isnum", IsNum
+                        "!", Not
+                    ]
+                do! spaces
+                let! expr = parseParens parseExpr
+                return op, expr
+            } |> parsePos (fun (op, expr) tag ->
+                Prim1Expr {
+                    Operator = op
+                    Expr = expr
+                    Tag = tag
+                })
+
+        let private parseIf =
+            parse {
+                do! skipString "if" >>. spaces
+                let! cond = parseExpr
+                do! spaces >>. skipChar ':' >>. spaces
+                let! trueBranch = parseExpr
+                do! spaces >>. skipString "else:" >>. spaces
+                let! falseBranch = parseExpr
+                return cond, trueBranch, falseBranch
+            } |> parsePos (fun (cond, trueBranch, falseBranch) tag ->
+                IfExpr {
+                    Condition = cond
+                    TrueBranch = trueBranch
+                    FalseBranch = falseBranch
+                    Tag = tag
+                })
+
+        let private parseBinding =
+            parse {
+                let! (ident, tag) = parseIdentifierName
+                do! spaces >>. skipChar '=' >>. spaces
+                let! expr = parseExpr
+                return {
+                    Identifier = ident
+                    Expr = expr
+                    Tag = tag   // identifier tag, not for entire binding
+                }
+            }
+
+        let private parseBindings =
+            sepBy1
+                (parseBinding .>> spaces)
+                (skipChar ',' .>> spaces)
+
+        let private parseLet =
+            parse {
+                do! skipString "let" >>. spaces
+                let! bindings = parseBindings
+                do! spaces >>. skipString "in" >>. spaces
+                let! expr = parseExpr
+                return bindings, expr
+            } |> parsePos (fun (bindings, expr) tag ->
+                LetExpr {
+                    Bindings = bindings
+                    Expr = expr
+                    Tag = tag
+                })
+
+        let private parseArguments =
+            sepBy
+                (parseExpr .>> spaces)
+                (skipChar ',' >>. spaces)
+
+        let private parseApplication =
+            parse {
+                let! (ident, _) = parseIdentifierName
+                do! spaces
+                let! args = parseParens parseArguments
+                return ident, args
+            }
+                |> parsePos (fun (ident, args) tag ->
+                    ApplicationExpr {
+                        Identifier = ident
+                        Arguments = args
+                        Tag = tag   // should we use the identifier's tag instead?
+                    })
+                |> attempt          // rollback if needed
+
+        let private parseSimpleExpr =
+            choice [
+                parseNumber
+                parseBool
+                parsePrim1
+                parseIf
+                parseLet
+                parseApplication
+                parseIdentifier   // must come after other parsers
+                parseParens parseExpr
+            ]
+
+        let private parseExprImpl =
+            let create op left right =
+                Prim2Expr {
+                    Operator = op
+                    Left = left
+                    Right = right
+                    Tag = fst left.Tag', snd right.Tag'
+                }
+            let parseOp =
+                choiceF [
+                    "+", create Plus
+                    "-", create Minus
+                    "*", create Times
+                    "&&", create And
+                    "||", create Or
+                    ">=", create GreaterEq   // must come before ">"
+                    ">", create Greater
+                    "<=", create LessEq      // must come before "<"
+                    "<", create Less
+                    "==", create Eq
+                ]
+            chainl1
+                (parseSimpleExpr .>> spaces)
+                (parseOp .>> spaces)
+
+        let parse = parseExpr
+
+        do parseExprRef.Value <- parseExprImpl
 
     let private parseText =
         spaces
-            >>. parseExpr
+            >>. Expr.parse
             .>> spaces
             .>> eof
 
@@ -210,5 +216,3 @@ module Parser =
         match runParserOnString parseText () "" text with
             | Success (result, _, _) -> Result.Ok result
             | Failure (msg, _, _) -> CompilerResult.error msg
-
-    do parseExprRef.Value <- parseExprImpl
