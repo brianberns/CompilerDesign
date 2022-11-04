d3b28e920359221460ee697bc604b189e406b1d3
Brian Berns <brianberns@outlook.com>
2022-11-04 17:30:18 -0400

    More work on type checking.

--- UnitTests/Assignment6/TaipanTests.fs
+++ UnitTests/Assignment6/TaipanTests.fs
@@ -67,7 +67,8 @@ type TaipanTests() =
                 "if true >= false: true else: !(true)", Error "Expected: Int, Actual: Bool"
                 "(add1(0) : Int)", Ok Type.int
                 "def plus(x : Int, y : Int) -> Int: x + y plus(3, 4)", Ok Type.int
-                "def plus(x : Int, y : Int) -> Int: x + y plus(true, 4)", Ok Type.int
+                "def plus(x : Int, y : Int) -> Int: x + y plus(true, 4)", Error "Expected: Int, Actual: Bool"
+                "def plus(x : Bool, y : Int) -> Int: x + y plus(true, 4)", Error "Expected: Int, Actual: Bool"
             ]
 
         for text, expected in pairs do
