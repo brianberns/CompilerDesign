63681af159f2655130f9f6d366196a6cbde19b3d
Brian Berns <brianberns@gmail.com>
2022-11-05 19:01:13 -0400

    More work on type inference.

--- Assignment6/TypeInfer.fs
+++ Assignment6/TypeInfer.fs
@@ -2,51 +2,63 @@
 
 open CompilerDesign.Core
 
-type private TypeEnvironment =
-    Map<IdentifierDef<unit>, Type<unit>>
-
-type private SchemeEnvironment =
-    Map<IdentifierDef<unit>, Scheme<unit>>
-
-type private Substitution =
-    List<IdentifierDef<unit> * Type<unit>>
-
 module TypeInfer =
 
     module private Type =
 
-        let rec substitute fromIdent toType = function
-            | TypeVariable ident as tv ->
-                if ident = fromIdent then toType
-                else tv
-            | TypeArrow def ->
-                TypeArrow {
-                    InputTypes =
-                        def.InputTypes
-                            |> List.map (substitute fromIdent toType)
-                    OutputType =
-                        def.OutputType
-                            |> substitute fromIdent toType
-                    Tag = def.Tag
-                }
-            | typ -> typ
+        let substitute fromIdent toType inType =
 
-        let rec apply subst typ =
+            let rec loop = function
+                | TypeVariable ident as tv ->
+                    if ident = fromIdent then toType
+                    else tv
+                | TypeArrow def ->
+                    TypeArrow {
+                        InputTypes = List.map loop def.InputTypes
+                        OutputType = loop def.OutputType
+                        Tag = def.Tag
+                    }
+                | typ -> typ
+
+            loop inType
+
+        let apply subst typ =
             (typ, subst)
                 ||> List.fold (fun acc (fromIdent, toType) ->
                     substitute fromIdent toType acc)
 
     module private Scheme =
 
-        let rec substitute fromIdent toType scheme =
-            if scheme.Identifiers |> List.contains fromIdent then
+        let substitute fromIdent toType scheme =
+            if List.contains fromIdent scheme.Identifiers then
                 scheme
-            else
-                {
-                    scheme with
-                        Type =
+            else {
+                scheme with
+                    Type =
+                        Type.substitute
+                            fromIdent
+                            toType
                             scheme.Type
-                                |> Type.substitute
-                                    fromIdent
-                                    toType
-                }
+            }
+
+        let apply subst scheme =
+            (scheme, subst)
+                ||> List.fold (fun acc (fromIdent, toType) ->
+                    substitute fromIdent toType acc)
+
+    type private Substitution =
+        List<IdentifierDef<unit> * Type<unit>>
+
+    type private TypeEnvironment =
+        Map<IdentifierDef<unit>, Type<unit>>
+
+    module TypeEnvironment =
+
+        let apply (subst : Substitution) (env : TypeEnvironment) =
+            (env, subst)
+                ||> List.fold (fun acc (fromIdent, toType) ->
+                    Map.map (fun _ typ ->
+                        Type.substitute fromIdent toType typ) acc)
+
+    type private SchemeEnvironment =
+        Map<IdentifierDef<unit>, Scheme<unit>>
