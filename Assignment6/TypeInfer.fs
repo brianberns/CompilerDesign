7ae7b2de5ec00bfd70b6ebbac37992f9c9659730
Brian Berns <brianberns@outlook.com>
2022-11-05 15:59:46 -0400

    More work on type inference.

--- Assignment6/TypeInfer.fs
+++ Assignment6/TypeInfer.fs
@@ -5,29 +5,48 @@ open CompilerDesign.Core
 type private TypeEnvironment =
     Map<IdentifierDef<unit>, Type<unit>>
 
-module TypeEnvironment =
-
-    let rec substitute (fromIdent : IdentifierDef<_>) (toType : Type<_>) = function
-        | TypeVariable ident as tv ->
-            if ident = fromIdent then toType
-            else tv
-        | TypeArrow def ->
-            TypeArrow {
-                InputTypes =
-                    def.InputTypes
-                        |> List.map (substitute fromIdent toType)
-                OutputType =
-                    def.OutputType
-                        |> substitute fromIdent toType
-                Tag = def.Tag
-            }
-        | typ -> typ
-
 type private SchemeEnvironment =
     Map<IdentifierDef<unit>, Scheme<unit>>
 
-module SchemeEnvironment =
-    ()
+type private Substitution =
+    List<IdentifierDef<unit> * Type<unit>>
 
 module TypeInfer =
-    ()
+
+    module private Type =
+
+        let rec substitute fromIdent toType = function
+            | TypeVariable ident as tv ->
+                if ident = fromIdent then toType
+                else tv
+            | TypeArrow def ->
+                TypeArrow {
+                    InputTypes =
+                        def.InputTypes
+                            |> List.map (substitute fromIdent toType)
+                    OutputType =
+                        def.OutputType
+                            |> substitute fromIdent toType
+                    Tag = def.Tag
+                }
+            | typ -> typ
+
+        let rec apply subst typ =
+            (typ, subst)
+                ||> List.fold (fun acc (fromIdent, toType) ->
+                    substitute fromIdent toType acc)
+
+    module private Scheme =
+
+        let rec substitute fromIdent toType scheme =
+            if scheme.Identifiers |> List.contains fromIdent then
+                scheme
+            else
+                {
+                    scheme with
+                        Type =
+                            scheme.Type
+                                |> Type.substitute
+                                    fromIdent
+                                    toType
+                }
