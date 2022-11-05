9fd2293bd4cae1746e84cf9adf64a737499525fa
Brian Berns <brianberns@gmail.com>
2022-11-04 23:53:57 -0400

    More work on type checking.

--- UnitTests/Assignment6/TaipanTests.fs
+++ UnitTests/Assignment6/TaipanTests.fs
@@ -70,7 +70,7 @@ type TaipanTests() =
                 "def plus(x : Int, y : Int) -> Int: x + y plus(true, 4)", Error "Expected: Int, Actual: Bool"
                 "def plus(x : Bool, y : Int) -> Int: x + y plus(true, 4)", Error "Expected: Int, Actual: Bool"
                 "def plus(x, y): x + y plus(3, 4)", Error "Missing type"
-                "let x : Int = 1, y : Int = 2 in (x + y : Int)", Ok Type.int
+                "let x : Int = 1, y : Int = x + 2 in (x + y : Int)", Ok Type.int
             ]
 
         for text, expected in pairs do
